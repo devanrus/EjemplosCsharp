aa775f9776080a5a50ff6c0912e9c3c2afe2e5bb
Angel Ruiz <angelruiz21@gmail.com>
2013-02-22 22:05:02 -0600

    EjemploBloqueaTeclas

---
+++ BloqueTeclas/BloqueTeclas/Bloquea.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;//Necesario para deshabilitar tecla windows
+using System.Diagnostics;//Necesario para deshabilitar tecla windows
+using System.Windows.Forms;
+
+namespace BloqueTeclas
+{
+    class Bloquea
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KBDLLHOOKSTRUCT
+        {
+            public Keys key;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public IntPtr extra;
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int id, LowLevelKeyboardProc callback, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hook);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hook, int nCode, IntPtr wp, IntPtr lp);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string name);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern short GetAsyncKeyState(Keys key);
+                       
+        private IntPtr ptrHook;
+        private LowLevelKeyboardProc objKeyboardProcess;
+
+        private IntPtr captureKey(int nCode, IntPtr wp, IntPtr lp)
+        {
+
+            if (nCode >= 0)
+            {
+                KBDLLHOOKSTRUCT objKeyInfo = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lp, typeof(KBDLLHOOKSTRUCT));
+
+                if (objKeyInfo.key == Keys.RWin || objKeyInfo.key == Keys.LWin) // Deshabilita tecla windows
+                {
+                    return (IntPtr)1;
+                }
+
+                if (objKeyInfo.key == Keys.Alt || objKeyInfo.key == Keys.F4 || objKeyInfo.key == Keys.Escape || objKeyInfo.key == Keys.Tab)//Deshabilita teclas alt+escape, alt+tab
+                {
+                    return (IntPtr)1;
+                }
+
+            }
+            return CallNextHookEx(ptrHook, nCode, wp, lp);
+        }
+
+        
+        public void bloc()
+        {
+            ProcessModule objCurrentModule = Process.GetCurrentProcess().MainModule;
+            objKeyboardProcess = new LowLevelKeyboardProc(captureKey);
+            ptrHook = SetWindowsHookEx(13, objKeyboardProcess, GetModuleHandle(objCurrentModule.ModuleName), 0);
+      
+        }
+
+    }
+}
